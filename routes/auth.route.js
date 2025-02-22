6a307c8d166db93365bba9343b53da9666dfda80
Abdullah An-Noor <aansourav@gmail.com>
2025-02-22 23:32:55 +0600

    Add authentication routes and dependencies
    - Integrate bcryptjs for password hashing
    - Add jsonwebtoken for authentication
    - Implement authentication route handlers (signup, signin, signout)
    - Update package dependencies to include authentication libraries

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,17 +1,16 @@
 import { Router } from "express";
+import { signIn, signOut, signUp } from "../controllers/auth.controller.js";
 
 const authRouter = Router();
 
-authRouter.post("/signup", (req, res) => {
-    res.send({ title: "signup" });
-});
+// /api/v1/auth/signup
+authRouter.post("/signup", signUp);
 
-authRouter.post("/signin", (req, res) => {
-    res.send({ title: "login" });
-});
+// /api/v1/auth/signin
+authRouter.post("/signin", signIn);
 
-authRouter.post("/signout", (req, res) => {
-    res.send({ title: "logout" });
-});
+// /api/v1/auth/signout
+authRouter.post("/signout", signOut);
 
 export default authRouter;
+ 
