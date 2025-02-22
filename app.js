c9b492628ae20f24ed836270f0b02a9ebcabc17b
Abdullah An-Noor <aansourav@gmail.com>
2025-02-22 21:49:07 +0600

    Add error handling middleware and request parsing to Express app
    - Integrate cookie-parser middleware
    - Add JSON and URL-encoded body parsing
    - Include global error handling middleware

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
+import cookieParser from "cookie-parser";
 import express from "express";
 import { PORT } from "./config/env.js";
-import authRouter from "./routes/auth.route.js";
-import userRouter from "./routes/user.route.js";
-import subscriptionRouter from "./routes/subscription.route.js";
 import connectDB from "./database/mongodb.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
+import authRouter from "./routes/auth.route.js";
+import subscriptionRouter from "./routes/subscription.route.js";
+import userRouter from "./routes/user.route.js";
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
+
+app.use(errorMiddleware);
 
 app.get("/", (req, res) => {
     res.send("Welcome to MySub - Subscription Tracker API");
