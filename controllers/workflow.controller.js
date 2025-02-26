4706cd05ab82ce8015d42d73e6e2eb86cd80ec73
aansourav <aansourav@gmail.com>
2025-02-26 11:52:24 +0600

    Implement subscription reminder workflow with Upstash Workflow
    - Add dayjs for date manipulation
    - Create sendReminders workflow function
    - Implement subscription fetching and reminder logic
    - Remove sensitive environment files
    - Update package dependencies

--- controllers/workflow.controller.js
+++ controllers/workflow.controller.js
@@ -1,8 +1,44 @@
+import dayjs from "dayjs";
 import { createRequire } from "module";
+import Subscription from "../model/subscription.model.js";
 const require = createRequire(import.meta.url);
 const { serve } = require("@upstash/workflow/express");
 
-export const sendReminders = serve(async (req, res) => {
-    console.log("Received request:", req.body);
-    res.send("Reminder sent");
+export const sendReminders = serve(async (context) => {
+    const REMINDER_DAYS = [7, 3, 1];
+
+    const { subscriptionId } = context.requestPayload;
+    const subscription = await fetchSubscription(context, subscriptionId);
+
+    if (!subscription || subscription.status !== "active") return;
+
+    const renewalDate = dayjs(subscription.renewalDate);
+
+    if (renewalDate.isBefore(dayjs())) {
+        console.log(
+            `Renewal date has passed for this subscription ${subscriptionId}. Stopping workflow `
+        );
+        return;
+    }
+
+    for (const daysBefore of REMINDER_DAYS) {
+        const reminderDate = renewalDate.subtract(daysBefore, "days");
+
+        if (reminderDate.isAfter(dayjs())) {
+        }
+    }
 });
+
+const fetchSubscription = async (context, subscriptionId) => {
+    return await context.run("get subscription", () => {
+        return Subscription.findById(subscriptionId).populate(
+            "user",
+            "name email"
+        );
+    });
+};
+
+const sleepUntilReminder = async (context, label, date) => {
+    console.log(`Sleeping until ${label} reminder at ${date}`);
+    await context.sleepUntil(label, date.toDate());
+};
--- routes/workflow.route.js
+++ routes/workflow.route.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 
 const workflowRouter = Router();
 
+// /api/v1/workflows - GET - Get all workflows
 workflowRouter.get("/", (req, res) => {
     res.send({ title: "WORKFLOW" });
 });
